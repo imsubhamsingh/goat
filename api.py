d6235836726b1061e0f0a8f74488cd812c293a3d
Subham Singh <geekysubham@gmail.com>
2021-06-12 01:45:24 +0530

    api: refactor response creation as a separate function

--- api.py
+++ api.py
@@ -10,7 +10,18 @@ class API:
         :param start_response
         """
         request = Request(environ)
-        response = Response()
-        response.text = "Hello, World, This is Goat"
+
+        response = self.handle_request(request)
 
         return response(environ, start_response)
+
+    def handle_request(self, request):
+        """
+        Method for response creation
+        """
+        user_agent = request.environ.get("HTTP_USER_AGENT", "NO Agent Found")
+
+        response = Response()
+        response.text = f"Hello Goat , with this user agent: {user_agent}"
+
+        return response
