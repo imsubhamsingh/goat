c4cfa933c76ed60b3d3ee7df505e0fb9f181ee89
Subham Singh <geekysubham@gmail.com>
2021-06-12 02:13:37 +0530

    api: implement route decorator and handler for storing routes

--- api.py
+++ api.py
@@ -2,6 +2,21 @@ from webob import Request, Response
 
 
 class API:
+    """
+    GOAT API
+    """
+
+    def __init__(self):
+        self.routes = {}
+
+    def route(self, path):
+        def wrapper(handler):
+            self.routes[path] = handler
+            print(self.routes)
+            return handler
+
+        return wrapper
+
     def __call__(self, environ, start_response):
         """
         Entrypoint callable receives three params
@@ -20,8 +35,13 @@ class API:
         Method for response creation
         """
         user_agent = request.environ.get("HTTP_USER_AGENT", "NO Agent Found")
-
         response = Response()
+
+        for path, handler in self.routes.items():
+            if path == request.path:
+                handler(request, response)
+                return response
+
         response.text = f"Hello Goat , with this user agent: {user_agent}"
 
         return response
--- app.py
+++ app.py
@@ -1,3 +1,19 @@
 from api import API
 
 app = API()
+
+
+"""
+Creating functions here to test out request
+handlers from different routes  using flash way :)
+"""
+
+
+@app.route("/index")
+def index(request, response):
+    response.text = "Hola from index page"
+
+
+@app.route("/about")
+def about(request, response):
+    response.text = "Namaste from about page :)"
