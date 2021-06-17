580cbe1496de5589d8b7588b09ca5620635c65db
Subham Singh <geekysubham@gmail.com>
2021-06-17 22:30:12 +0530

    orm: implement table operations

--- orm.py
+++ orm.py
@@ -4,7 +4,7 @@
 #            (VERSION 1.0)              #
 #                                       #
 #########################################
-
+import inspect
 import sqlite3
 
 
@@ -35,29 +35,128 @@ class Database:
 
 
 class Table:
-    def get_attributes(self):
-        pass
+    """
+    Table operations
+    """
 
-    def set_attributes(self):
-        pass
+    def __init__(self, **kwargs):
+        self._data = {"id": None}
+        for key, value in kwargs.items():
+            self._data[key] = value
 
-    def insert_sql(self):
-        pass
+    def __get_attribute__(self, key):
+        _data = super().__getattribute__("_data")
+        if key in _data:
+            return _data[key]
+        return super().__getattribute__(key)
 
-    def select_sql(self):
-        pass
+    def __setattr__(self, key, value):
+        super().__setattr__(key, value)
+        if key in self._data:
+            self._data[key] = value
 
-    def select_all_sql(self):
-        pass
+    @classmethod
+    def _get_create_sql(cls):
 
-    def select_where_sql(self):
-        pass
+        CREATE_TABLE_SQL = "CREATE TABLE IF NOT EXISTS {name} ({fields});"
+        fields = ["id INTEGER PRIMARY KEY AUTOINCREMENT"]
 
-    def update_sql(self):
-        pass
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(f"{name} {fields.sql_type}")
+            elif isinstance(field, ForeignKey):
+                fields.append(f"{name}_id INTEGER")
 
-    def delete_sql(self):
-        pass
+        fields = ",".join(fields)
+        name = cls.__name__.lower()
+        return CREATE_TABLE_SQL.format(name=name, fields=fields)
+
+    def _get_insert_sql(self):
+
+        INSERT_SQL = "INSERT INTO {name} ({fields}) VALUES ({placeholders});"
+        cls = self.__class__
+        fields = []
+        placeholders = []
+        values = []
+
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+                values.append(getattr(self, name))
+                placeholders.append("?")
+            elif isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+                values.append(getattr(self, name).id)
+                placeholders.append("?")
+
+        fields = ",".join(fields)
+        placeholders = ",".join(placeholders)
+
+        sql = INSERT_SQL.format(
+            name=cls.__name__.lower(), fields=fields, placeholders=placeholders
+        )
+
+        return sql, values
+
+    @classmethod
+    def _get_select_all_sql(cls):
+
+        SELECT_ALL_SQL = "SELECT {fields} FROM {name};"
+
+        fields = ["id"]
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+            if isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+
+        sql = SELECT_ALL_SQL.format(name=cls.__name__.lower(), fields=",".join(fields))
+
+    @classmethod
+    def get_select_where_sql(cls):
+
+        SELECT_WHERE_SQL = "SELECT {fields} FROM {name} WHERE id= ?;"
+        fields = ["id"]
+
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+            if isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+
+        sql = SELECT_WHERE_SQL.format(name=cls.__name.lower(), fields=",".join(fields))
+        params = [id]
+
+        return sql, fields, params
+
+    def _get_update_sql(self):
+        UPDATE_SQL = "UPDATE {name} SET {fields} WHERE id = ?"
+        cls = self.__class__
+        fields = []
+        values = []
+
+        for name, field in inspect.getmembers(cls):
+            if isinstance(field, Column):
+                fields.append(name)
+                values.append(getattr(self, name))
+            elif isinstance(field, ForeignKey):
+                fields.append(name + "_id")
+                values.append(getattr(self, name).id)
+        values.append(getattr(self, "id"))
+
+        sql = UPDATE_SQL.format(
+            name=cls.__name__.lower(),
+            fields=",".join([f"{field} = ?" for field in fields]),
+        )
+
+        return sql, values
+
+    def _get_delete_sql(cls, id):
+        DELETE_SQL = "DELETE FROM {name} WHERE id = ?"
+
+        sql = DELETE_SQL.format(name=cls.__name__.lower())
+
+        return sql, [id]
 
 
 class Column:
@@ -67,6 +166,17 @@ class Column:
 
     def __init__(self, column_name):
         self.column_name = column_name
+
+    @property
+    def sql_type(self, type):
+        SQL_TYPE_MAP = {
+            int: "INTEGER",
+            float: "FLOAT",
+            str: "TEXT",
+            bytes: "BLOB",
+            bool: "INTEGER",
+        }
+        return SQL_TYPE_MAP[self.type]
 
 
 class ForeignKey:
